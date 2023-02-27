d60d4042947474c0aff1a7eb45ac8367ddc4c759
NjambiW <126060339+NjambiW@users.noreply.github.com>
2023-02-27 16:03:24 +0300

    an added a high score to check the highest score in the game

--- snake.py
+++ snake.py
@@ -40,6 +40,13 @@ class Snake:
 
         self.head.forward(MOVE_DISTANCE)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def up(self):
         """moves the snake upwards"""
         if self.head.heading() != DOWN:
