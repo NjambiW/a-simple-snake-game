a26da79ab0fb821033e75cb98bd272b508caf42b
NjambiW <126060339+NjambiW@users.noreply.github.com>
2023-02-27 12:46:20 +0300

    a simple snake game using the turtle module

---
+++ scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("courier", 12, "normal")
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER ", align="center", font=("courier", 24, "normal"))
+
+    def new_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
+
+
+
+
---
+++ snake.py
@@ -0,0 +1,61 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (20, 0), (40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    """contains a list of positions and segments of the snake"""
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        snake_segment = Turtle(shape="square")
+        snake_segment.color("white")
+        snake_segment.penup()
+        snake_segment.goto(position)
+        self.segments.append(snake_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        """makes sure all the segments move together
+        to create a singular movement of the snake"""
+        for segment_number in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[segment_number - 1].xcor()
+            new_y = self.segments[segment_number - 1].ycor()
+            self.segments[segment_number].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """moves the snake upwards"""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """moves the snake downwards"""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """moves the snake to the left"""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """moves the snake to the right"""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
